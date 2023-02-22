2a50980e5d074e8ad401770fd670d1eea4afd54d
renoob <98941290+renoob21@users.noreply.github.com>
2023-02-22 16:45:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const db = knex({
     client:'pg',
     connection: {
         host: '127.0.0.1',
-        user: 'postgres',
-        password: '1ffahTsurayy@',
+        user: '',
+        password: '',
         database: 'facerecognition'
     }
 })
